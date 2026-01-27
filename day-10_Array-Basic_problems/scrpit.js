df566b2a5430f9674e1e032eb7a82bc55d01b406
Deepak Vats <deepakvats123123@gmail.com>
2026-01-27 14:51:17 +0530

    added solution

--- day-10_Array-Basic_problems/scrpit.js
+++ day-10_Array-Basic_problems/scrpit.js
@@ -57,6 +57,112 @@ function sortInAscending(arr){
 // * **Output:** `[1, 2, 3, 4]`
 
 function removeDuplicates(arr){
-    
+    // Good approch for interview also
+    // let resObj = {}
+    // for(let i=0; i<arr.length; i++){
+    //     if(resObj[arr[i]]){
+    //        resObj[arr[i]] += 1;
+    //     }
+    //     else{
+    //         resObj[arr[i]] = 1;
+    //     }
+    // }
+    // return Object.keys(resObj).map(el=> Number(el))
+
+
+    // another approch using arr.filter() and arr.indexOf()
+    return arr.filter((el, i) => i === arr.indexOf(el));
 }
-console.log(removeDuplicates([1, 2, 2, 3, 3, 4]));
+// console.log(removeDuplicates([1, 2, 2, 3, 3, 4]));
+
+// ### 5️⃣ Merge Two Arrays Without Using concat or spread
+// * **Input:** ``
+// * **Output:** `[1, 2, 3, 4, 5]`
+
+function mergeArr (arr1, arr2){
+    // using spred opratar the best approch
+    return [...arr1, ...arr2]
+
+    //using inbuit method
+    // return arr1.concat(arr2);
+
+    // this is another approch without loop
+    // return arr1.push(...arr2)
+
+    // using loop
+    // for(let i=0; i<arr2.length; i++){
+    //     arr1.push(arr2[i])
+    // }
+    // return arr1
+
+
+}
+// console.log(mergeArr([1, 2, 3], [4, 5]));
+
+
+// ### 1️⃣ Find the Second Largest Element
+// * **Input:** `[10, 20, 4, 45, 99]`
+// * **Output:** `45`
+
+function secondLargest(arr){
+    let largest = arr[0]
+    let secLargest = arr[0]
+
+    for(let i=0; i<arr.length; i++){
+        
+        if(largest < arr[i]){
+            secLargest = largest;
+            largest = arr[i]
+        }
+        else if(arr[i] > secLargest && arr[i] !== largest){
+            secLargest = arr[i];
+        }
+    }
+    return secLargest
+}
+// console.log(secondLargest([10, 20, 4, 45, 99]));
+
+// ### 2️⃣ Rotate an Array to the Right by K Steps
+// * **Input:** `[1, 2, 3, 4, 5]`, `K = 2`
+// * **Output:** `[4, 5, 1, 2, 3]`
+
+
+
+
+
+
+
+
+
+// ### 3️⃣ Check if Two Arrays Are Equal (Same Order)
+// * **Input:** `[1, 2, 3]` vs `[1, 2, 3]`
+// * **Output:** `Equal`
+
+function areEqual(arr1, arr2){
+    for(let i=0; i<arr1.length; i++){
+        if(arr1[i] !== arr2[i]){
+            return "Not Equal"
+        }
+    }
+    return "Equal"
+}
+// console.log(areEqual([1, 2, 3], [1, 2, 3]));
+
+// ### 4️⃣ Count Even and Odd Numbers in an Array
+// * **Input:** `[2, 5, 7, 8, 10]`
+// * **Output:** `Even = 3`, `Odd = 2
+
+function CountEvenOdd(arr){
+    let Evens = 0;
+    let Odds = 0;
+    for(let i=0; i<arr.length; i++){
+        if(arr[i]%2 === 0){
+            Evens++;
+        }
+        else{
+            Odds++
+        }
+    }
+    return `Even = ${Evens} Odd = ${Odds}`
+}
+console.log(CountEvenOdd([2, 5, 7, 8, 10]));
