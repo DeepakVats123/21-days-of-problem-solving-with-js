76d4c2ea4b25853c44bbf04e8d60515c0d13151b
Deepak Vats <deepakvats123123@gmail.com>
2026-03-23 19:40:28 +0530

    added recursion Object poblems

--- day-19_Recursion_On_Objects/scrpit.js
+++ day-19_Recursion_On_Objects/scrpit.js
@@ -60,27 +60,96 @@ function printAllValues (obj){
 // ```
 // - **Output:** `6`
 
-function countKeys(obj, count=0){
+function countKeys(obj){
+ let count = 0;
+    for(let key in obj){
+        count++
+        if(typeof obj[key] === 'object' &&  obj[key] !== null){
+           count += countKeys(obj[key])
+        }
+    }
+    return count
+}
+// console.log(countKeys({
+//   a: 1,
+//   b: {
+//     c: 2,
+//     d: {
+//       e: 3,
+//       f: 4
+//     }
+//   }
+// }))
 
-    let keys = Object.keys(obj) 
-    for(let i=0; i< keys.length; i++){
-        if(i === keys.length-1) return count;
-        if(typeof obj[i] !== 'object'){
-            count++;
+// ### **3️⃣ Find the Sum of All Numeric Values in a Nested Object**
+// - **Input:**
+
+// ```js
+// {
+//   x: 10,
+//   y: {
+//     z: 5,
+//     w: {
+//       k: 15
+//     }
+//   }
+// }
+// ```
+// - **Output:** `30`
+
+function FindSumOfAllNumbers(obj, sum=0){
+    
+    for(let key in obj){
+        if(typeof obj[key] === 'number'){
+            sum += obj[key];
         }
-        else{
-            
-            countKeys(obj[i], count +1);
+        else if(typeof obj[key] === 'object' && obj[key] !== null){
+            sum +=  FindSumOfAllNumbers(obj[key])
         }
+        
+    }
+    return sum
+
+}
+// console.log(FindSumOfAllNumbers({
+//   x: 10,
+//   y: {
+//     z: 5,
+//     w: {
+//       k: 15
+//     }
+//   }
+// }));
+
+
+// ### **4️⃣ Print All Keys in a Nested Object**
+// - **Input:**
+// ```js
+// {
+//   a: {
+//     b: {
+//       c: 1;
+//     }
+//   }
+// }
+// ```
+// - **Output:** `a b c`
+
+function printAllKeys(obj){
+    for(let key in obj){
+        if(typeof obj[key] === 'object' && obj[key] !== null){
+            console.log(key);
+            printAllKeys(obj[key])
+        }else{
+            console.log(key)
+        }
+        
     }
 }
-console.log(countKeys({
-  a: 1,
-  b: {
-    c: 2,
-    d: {
-      e: 3,
-      f: 4
+printAllKeys({
+  a: {
+    b: {
+      c: 1
     }
   }
-}))
+})
